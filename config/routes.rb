a82e5155c97f27a68d12d137f07ec5dd1330c678
EC2 Default User <ec2-user@ip-172-31-19-174.ap-northeast-1.compute.internal>
2022-07-07 22:20:45 +0000

    [Finish]4-7 ユーザー認証機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # daviceを使用する時にusersを含むことを意味している
   devise_for :users
   root to: 'homes#top'
+  get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
