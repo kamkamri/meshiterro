b19a25f66c557d87cdf55e8844404bd80c380142
EC2 Default User <ec2-user@ip-172-31-19-174.ap-northeast-1.compute.internal>
2022-07-10 08:21:02 +0000

    [Finish]4-15 ユーザーに関する機能2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    # アソシエーション関連を持っているモデル同士の記述方法
+    # 複数形になっているのはhas_many :post_images　になっているから
+    # ユーザーが投稿した投稿
+    @post_images = @user.post_images
   end
 
   def edit
